418145828c370441ada5c5c9c346086596eb1389
MadhuriYadav2521 <madhuri.y.0932@gmail.com>
2023-09-05 22:34:33 +0530

    accordion

--- frontend/src/components/OrderHistory.jsx
+++ frontend/src/components/OrderHistory.jsx
@@ -1,26 +1,26 @@
-import "bootstrap/dist/css/bootstrap.min.css"; // Import Bootstrap CSS
-import "bootstrap/dist/js/bootstrap.bundle.min.js"; // Import Bootstrap JavaScript (bundle includes Popper.js)
 
-
+// import "../components/style.css"
+import Navbar from "./Navbar";
+import "../components/accordian.css"
 const OrderHistory = () => {
     return (
-        <div className="container">
+        <div>
+            <Navbar />
 
-            <div class="accordion" id="accordionExample">
-                <div class="accordion-item">
-                    <h2 class="accordion-header" id="headingOne">
-                        <button class="accordion-button" type="button" data-bs-toggle="collapse" data-bs-target="#collapseOne" aria-expanded="true" aria-controls="collapseOne">
-                            Accordion Item #1
-                        </button>
-                    </h2>
-                    <div id="collapseOne" class="accordion-collapse collapse show" aria-labelledby="headingOne" data-bs-parent="#accordionExample">
-                        <div class="accordion-body">
-                            <strong>This is the first item's accordion body.</strong> It is shown by default, until the collapse plugin adds the appropriate classes that we use to style each element. These classes control the overall appearance, as well as the showing and hiding via CSS transitions. You can modify any of this with custom CSS or overriding our default variables. It's also worth noting that just about any HTML can go within the <code>.accordion-body</code>, though the transition does limit overflow.
-                        </div>
+            <div className="container">
+                <details>
+                    <summary>Accordion Item 1</summary>
+                    <div>
+                        <h2>Content for Accordion Item 1</h2>
                     </div>
-                </div>
+                </details>
+                <details>
+                    <summary>Accordion Item 2</summary>
+                    <div>
+                        <h2>Content for Accordion Item 2</h2>
+                    </div>
+                </details>
             </div>
-
         </div>
     );
 }
